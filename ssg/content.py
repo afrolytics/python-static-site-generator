06b44653af064c488d52f3b8a7173a4f7db43819
afrolytics <91022835+afrolytics@users.noreply.github.com>
2022-12-23 20:14:08 -0700

    module 3.1

---
+++ ssg/content.py
@@ -0,0 +1,10 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    def load(self):
